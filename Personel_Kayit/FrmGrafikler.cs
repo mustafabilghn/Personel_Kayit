fd2ece1d00912dbbb2b791dba7c7c3d2beb57dc0
Mustafa Bilgehan Ateş <146623209+mustafabilghn@users.noreply.github.com>
2025-07-01 14:20:40 +0300

    Images added.

--- Personel_Kayit/FrmGrafikler.cs
+++ Personel_Kayit/FrmGrafikler.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace Personel_Kayit
+namespace Personel_Kayit    
 {
     public partial class FrmGrafikler : Form
     {
